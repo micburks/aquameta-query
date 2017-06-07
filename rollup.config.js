f67c616c5d7607384ef2cd4d57327cd4af966e11
Mickey Burks <mickey@aquameta.com>
2017-06-07 08:20:53 -0500

    Add uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import eslint from 'rollup-plugin-eslint'
+import uglify from 'rollup-plugin-uglify'
 
 export default {
   entry: 'src/index.js',
@@ -12,6 +13,7 @@ export default {
     'location'
   ],
   plugins: [
+    uglify(),
     eslint(),
     babel(),
     resolve({
